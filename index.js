1a43b0ba585e4738b09c34d69fe4c3c8de181a80
juliaacastellar <castellar2003j@gmail.com>
2023-12-03 15:03:48 -0300

    conexão com MySQL finalizada

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
 const app = express()
+const mysql = require("mysql2")
 
 //rotas
 app.engine("handlebars", exphbs.engine())
@@ -10,5 +11,22 @@ app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     res.render("home")})
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")})
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database:"todoapp",
+    port: 3306
+})
+
+conexao.connect((erro) => {
+    if (erro) {
+        return console.log(erro)
+    }
+
+    console.log ("Estou conectado ao MySQL")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000")})
+})
