458627e1a1dec00726c11614e5bcf64e37272933
juliaacastellar <castellar2003j@gmail.com>
2023-12-03 16:00:53 -0300

    salvando tarefas no banco de dados

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const exphbs = require("express-handlebars")
 const app = express()
 const mysql = require("mysql2")
 
-//rotas
+//
 app.engine("handlebars", exphbs.engine())
 app.set("view engine", "handlebars")
 
 app.use(express.static("public"))
+
+//para converter dados do formulaŕio em objetos JS
+app.use(express.urlencoded ({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (req, res) => {
+    const descricao = req.body.descricao 
+    const completa = 0
+
+    const sql =`
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) =>{
+        if (erro) {
+            return console.log(erro)    
+        }
+        res.redirect('/')
+    })
+})
 
 app.get('/', (req, res) => {
     res.render("home")})
@@ -17,8 +42,7 @@ const conexao = mysql.createConnection({
     user: "root",
     password: "password",
     database:"todoapp",
-    port: 3306
-})
+    port: 3306 })
 
 conexao.connect((erro) => {
     if (erro) {
