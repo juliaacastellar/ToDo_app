2c67c2ca5d9cf341947f9b80c52102e3364b7435
Julia Castellar <castellar2003j@gmail.com>
2023-12-03 22:57:05 -0300

    filtro de tarefas completas, completo.

--- index.js
+++ index.js
@@ -69,6 +69,28 @@ app.post('/criar', (req, res) => {
     })
 })
 
+app.get('/completas', (req, res)=>{
+    const sql = `
+    SELECT * FROM tarefas
+    WHERE completa = 1
+    `
+    conexao.query(sql, (erro, dados)=>{
+        if (erro) {
+            return console.log(erro)
+        }
+        const tarefas = dados.map((dado) => {
+            return{
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: true 
+            }
+        })
+
+        const quantidadeTarefas = tarefas.length
+        res.render('completas', {tarefas, quantidadeTarefas})
+    })
+})
+
 app.get('/ativas', (req, res)=>{
     const sql = `
     SELECT * FROM tarefas
