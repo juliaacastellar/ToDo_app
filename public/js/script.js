50496df1cf5e8cc7c6451a9cdb74236699f84eb5
Julia Castellar <castellar2003j@gmail.com>
2023-12-03 23:07:03 -0300

    exclusao de tarefas individuais

--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,18 @@ function completarTarefa(id){
     window.location.reload() /*pra recarregar a pagina*/
 }
 
+function excluirTarefa(id){
+    fetch("http://localhost:3000/excluir", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id})  
+    })
+
+    window.location.reload() /*pra recarregar a pagina*/
+}
+
 function descompletarTarefa(id){
     fetch("http://localhost:3000/descompletar", {
         method: "POST",
